d3efe4702cff0c3e3f7a7aa1e46dd60d8d480f70
reasv <7143787+reasv@users.noreply.github.com>
2024-12-10 14:28:24 +0100

    Refactor and reorganize

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from clean import clean_all
+from condense import condense_all
+load_dotenv()
+
+if __name__ == "__main__":
+    target_user_id = os.getenv("TARGET_USER_ID")
+    assert target_user_id, "TARGET_USER_ID is not set"
+    target_names = os.getenv("TARGET_NAMES", "").split(",")
+    target_name_replacement = os.getenv("TARGET_NAME_REPLACEMENT", "assistant")
+
+    clean_all(target_user_id, target_names, target_name_replacement)
+    condense_all(target_name_replacement)
